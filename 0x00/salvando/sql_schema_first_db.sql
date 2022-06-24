a027d42347f894f6e97558fe1b584eb7b73a08ac
Letícia Leonel Rodrigues <CIANDT\leticial@lnb036111cps.ciandt.global>
2022-06-24 16:03:51 -0300

    Task 4 - DB

--- 0x00/salvando/sql_schema_first_db.sql
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -1,6 +1,14 @@
+-- TABLE
 CREATE TABLE cliente (
 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
 nome VARCHAR(80) NOT NULL, 
 email VARCHAR(40) NOT NULL,
 telefone INT
-)
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
