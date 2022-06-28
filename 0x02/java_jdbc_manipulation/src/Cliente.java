fd19f0886a01cedbd1aef41644f2aa2a90b2a38d
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-06-28 10:41:54 -0300

    Task 0 - JDBC CRUD

---
+++ 0x02/java_jdbc_manipulation/src/Cliente.java
@@ -0,0 +1,45 @@
+public class Cliente {
+    private Integer id, idade;
+    private String nome, CPF, RG;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+}
