db48d6099f4ed0c5183104416edaf0cd052cec90
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-06-29 13:48:44 -0300

    Task 0 - Parte III

---
+++ jpa_hibernate/sql_schema_database_admin.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE Pessoa (id  integer, cpf varchar(255), dataNascimento date, email varchar(255), idade integer, nome varchar(255), primary key (id));
+CREATE TABLE Produto (id  integer, nome varchar(255), preco double precision, quantidade integer, status boolean, primary key (id));
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
